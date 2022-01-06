c208a447033675338068ba3c130673964d9d852f
theo-grivel <togriv@gmail.com>
2022-01-06 14:54:59 +0100

    update ventoy

--- download_ventoy.sh
+++ download_ventoy.sh
@@ -1,2 +1,7 @@
 wget https://github.com/ventoy/Ventoy/releases/download/v1.0.63/ventoy-1.0.63-linux.tar.gz
 tar -xvzf ventoy-1.0.63-linux.tar.gz
+cd ventoy-1.0.63
+sudo ./Ventoy2Disk.sh -i /dev/sdc
+sudo apt install exfat-fuse
+
+# add your os in the folder ventoy
